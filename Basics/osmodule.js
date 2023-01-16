6a35a99ad3774eb92917dbd15b00d4e9d8aeb1ab
Teuton Roy <popeyehk404@gmail.com>
2023-01-16 19:26:19 +0530

    About os_module

---
+++ Basics/osmodule.js
@@ -0,0 +1,31 @@
+//import os module//
+
+const os = require('os');
+
+//using some of the methods//
+console.log(os.freemem()); //this will give the free memory in the system//
+
+console.log(os.totalmem()); //this will give the total memory in the system//
+
+console.log(os.homedir()); //this will give the home directory of the system//
+
+console.log(os.hostname()); //this will give the hostname of the system//
+
+console.log(os.networkInterfaces()); //this will give the network interfaces of the system//
+
+console.log(os.platform()); //this will give the platform of the system//
+
+console.log(os.release()); //this will give the release of the system//
+
+console.log(os.tmpdir()); //this will give the tmpdir of the system//
+
+console.log(os.type()); //this will give the type of the system//
+
+console.log(os.userInfo()); //this will give the user info of the system//
+
+console.log(os.uptime()); //this will give the uptime of the system//
+
+console.log(os.arch()); //this will give the architecture of the system//
+
+console.log(os.cpus()); //this will give the cpus of the system//
+
