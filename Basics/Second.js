37d34033f69f9e290edf1ae8d59421dcd31c6645
Teuton Roy <popeyehk404@gmail.com>
2023-01-16 18:49:30 +0530

    How to import-export from Second.js to index.js AND add about what Commonjs module system.

---
+++ Basics/Second.js
@@ -0,0 +1,26 @@
+// This file exports the function//
+
+// This is the function we want to export//
+
+MyInfo = {
+    name: 'Teuton Roy',
+    age: 23,
+    location: 'West Bengal',
+    occupation: 'Student',
+    Collage: 'NIT Jamshepur',
+    Branch: 'Computer Science and Engineering',
+    Course: 'MCA',
+    Year: 'Second Year',
+    Roll: '2021PGCACA070',
+    Email: '2021pgcaca070@nitjsr.ac.in',
+    Phone: '8972028100',
+    Developer: 'true'
+}
+
+module.exports = MyInfo;
+
+//Now we are going to import this MyInfo object in index.js//
+
+/*
+    This [second.js] called [CommonJS module system] : which is refers some contant from this file and export it to another file.
+*/
--- Basics/index.js
+++ Basics/index.js
@@ -1,1 +1,8 @@
 console.log("Hello World");
+
+//now, in index.js we want to access the function in the other file//
+
+//we are going to export from Second.js//
+
+const MyInfo = require('./Second.js'); //this is the path to the file we want to import//
+console.log(MyInfo);
