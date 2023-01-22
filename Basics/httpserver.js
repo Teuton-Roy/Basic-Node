380ab6db62a4e3d817f81c610cac6ba82beb439d
Teuton Roy <popeyehk404@gmail.com>
2023-01-22 18:57:05 +0530

    ..

--- Basics/httpserver.js
+++ Basics/httpserver.js
@@ -1,6 +1,6 @@
-const http  = require('http');
+const http = require('http');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
@@ -8,6 +8,6 @@ const server = http.createServer((req, res) => {
     res.end('<h1>Hello Boobs!</h1>');
 
 });
-server.listen(() => {
-    console.log('Server is running on port ${port}');
+server.listen(port, () => {
+    console.log("server is running on port ${port}");
 });
