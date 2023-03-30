96428913039438ea435f22c4f3eacd98275801bc
Teuton Roy <popeyehk404@gmail.com>
2023-03-30 18:18:49 +0530

    listen on the app on the port 8080

--- mycontact-backend/index.js
+++ mycontact-backend/index.js
@@ -7,3 +7,8 @@ const app = express()
 
 //define a port
 const port = 8080
+
+//now we can have listen on the app on the port 8080
+app.listen(port, () =>{ //callback function
+    console.log(`Server is running on port ${port}`);
+})
