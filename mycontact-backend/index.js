509736b171c4535bffd480a630869c9b616f4751
Teuton Roy <popeyehk404@gmail.com>
2023-03-30 18:35:46 +0530

    In this project we are going to have an environment variable file where we are going to define the port so we install DOTENV package (npm i dotenv). And this going to give us an access to fatch value of the port from the environment variable file with the help of a process module(const port = process.env.PORT || 8080).

--- mycontact-backend/index.js
+++ mycontact-backend/index.js
@@ -1,14 +1,14 @@
 //create s express server   
-
 const express = require('express')
+const dotenv = require('dotenv').config()
 
 //create an express app
 const app = express()
 
-//define a port
-const port = 8080
+//define a port (const port = 8080)
+const port = process.env.PORT || 8080
 
 //now we can have listen on the app on the port 8080
 app.listen(port, () =>{ //callback function
     console.log(`Server is running on port ${port}`);
-})
+}) 
