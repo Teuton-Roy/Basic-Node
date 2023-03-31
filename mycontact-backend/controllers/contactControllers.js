5730516f3c2aadcde9dd1e1cef4932509f7c764e
Teuton Roy <popeyehk404@gmail.com>
2023-03-31 13:42:07 +0530

    create getContact, updateContact controllers and import in contacRoute

--- mycontact-backend/controllers/contactControllers.js
+++ mycontact-backend/controllers/contactControllers.js
@@ -2,7 +2,7 @@
 //@Route: GET /api/contacts
 //@Access: Public
 
-const getContact =  (req, res) => {
+const getContacts =  (req, res) => {
     res.status(200).json({message: 'Get all contacts'})
 };
 
@@ -13,6 +13,22 @@ const createContact = (req, res) => {
     res.status(201).json({message: 'Create contact'})
 }
 
+//@Description: Get Contact
+//@Route: GET /api/contacts
+//@Access: Public
+const getContact = (req, res) => {
+    res.status(200).json({message: `Get contact for ${req.params.id}`})
+}
+
+//@Description: Update Contact
+//@Route: PUT /api/contacts
+//@Access: Public
+const updateContact = (req, res) => {
+    res.status(200).json({message: `Update contact ${req.params.id}`})
+}
+
 //exprot the controller functions
-module.exports = getContact
+module.exports = getContacts
 module.exports = createContact
+module.exports = getContact
+module.exports = updateContact
--- mycontact-backend/routes/contactRoutes.js
+++ mycontact-backend/routes/contactRoutes.js
@@ -1,21 +1,24 @@
 const express = require('express')
 const router = express.Router()
-//import the get contact
-const getContact = require('../controllers/contactControllers')
+
+//import the get contacts
+const getContacts = require('../controllers/contactControllers')
 //import the create contact
 const createContact = require('../controllers/contactControllers')
+//import the get contact
+const getContact = require('../controllers/contactControllers')
+//import the update contact
+const updateContact = require('../controllers/contactControllers')
 
 //config the routes
-router.route('/').get(getContact)
+router.route('/').get(getContacts)
 
 //route for the post request to create a contact
 router.route('/').post(createContact);
 
 
 //route for the get request for individual contact
-router.route('/:id').get((req, res) => {
-    res.status(200).json({message: `Get contact for ${req.params.id}`})
-});
+router.route('/:id').get(getContact);
 
 
 //route for the put request to update a contact
@@ -23,9 +26,7 @@ router.route('/:id').get((req, res) => {
 params is a property of the request object
 req.params.id is the id of the contact to be updated
 */
-router.route('/:id').put((req, res) => {
-    res.status(200).json({message: `Update contact ${req.params.id}`})
-});
+router.route('/:id').put(updateContact);
 
 
 //route for the delete request to delete a contact
