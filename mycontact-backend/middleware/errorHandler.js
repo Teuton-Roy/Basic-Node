a130e7bb2284e318a24b688093f30e330d4847a2
Teuton Roy <popeyehk404@gmail.com>
2023-03-31 15:17:04 +0530

    use the constants in errorhandler

--- mycontact-backend/middleware/errorHandler.js
+++ mycontact-backend/middleware/errorHandler.js
@@ -1,16 +1,29 @@
+//import constants
+const constants = require('../routes/constants')
+
 //create constant error handler
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500
 
     //set the status code using switch case
     switch (statusCode) {
-        case 400:
+        case constants.VALIDATION_ERROR:
             res.json({title:"Validation Failed",message: err.message, stackTrace: err.stack})
             break;
-        case 404:
+        case constants.NOT_FOUND :
             res.json({title:"Not Found!",message: err.message, stackTrace: err.stack})
+        
+        case constants.UNAUTHORIZED :
+            res.json({title:"Unauthorized",message: err.message, stackTrace: err.stack})
+        
+        case constants.FORBIDDEN :
+            res.json({title:"Forbidden",message: err.message, stackTrace: err.stack})
+        
+        case constants.INTERNAL_SERVER_ERROR :
+            res.json({title:"Server_Error",message: err.message, stackTrace: err.stack})
 
         default:
+            console.log("No Error, Everything is fine!");
             break;
     }
     //stackTrace is the stack trace of the error object means the line of code where the error occured.
--- mycontact-backend/routes/constants.js
+++ mycontact-backend/routes/constants.js
@@ -1,4 +1,4 @@
-exportS.constants = {
+exports.constants = {
     VALIDATION_ERROR: 400,
     UNAUTHORIZED: 401,
     FORBIDDEN: 403,
