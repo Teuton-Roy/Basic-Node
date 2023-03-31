6eb912e4d4f45946c65045e0719d30a932d61cd9
Teuton Roy <popeyehk404@gmail.com>
2023-03-31 14:46:45 +0530

    Transform the errorHandler

--- mycontact-backend/middleware/errorHandler.js
+++ mycontact-backend/middleware/errorHandler.js
@@ -1,6 +1,8 @@
 //create constant error handler
 const errorHandler = (err, req, res, next) => {
-    
+    const statusCode = res.statusCode ? res.statusCode : 500
+    res.json({message: err.message, stackTrace: err.stack})
+    //stackTrace is the stack trace of the error object means the line of code where the error occured.
 }
 
 //export the error handler
