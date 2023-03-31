6ff781c5be4c1243195876a0b2f7b4bc68c2f42f
Teuton Roy <popeyehk404@gmail.com>
2023-03-31 15:36:54 +0530

    async wrap in asyncHandler function. So, whenever an exception occurred going to pass it to the errorHandler file

--- mycontact-backend/controllers/contactControllers.js
+++ mycontact-backend/controllers/contactControllers.js
@@ -4,14 +4,14 @@ const asyncHandler = require('express-async-handler')
 //@Description: Get All Contacts
 //@Route: GET /api/contacts
 //@Access: Public
-const getContacts = async (req, res) => {
+const getContacts =asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Get all contacts'})
-};
+})
 
 //@Description: create new Contacts
 //@Route: POST /api/contacts
 //@Access: Public
-const createContact = async (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
     console.log("The request body is: ", req.body);
     //Destructure the request body
     const {name, email, phone} = req.body
@@ -20,7 +20,7 @@ const createContact = async (req, res) => {
         throw new Error('Please provide name, email and phone!')
     }
     res.status(201).json({message: 'Create contact'})
-}
+})
 
 //@Description: Get Contact
 //@Route: GET /api/contacts/:id
@@ -30,23 +30,23 @@ const createContact = async (req, res) => {
 params is a property of the request object.
 req.params.id is the id of the contact to be updated/delete/get etc.
 */
-const getContact =async (req, res) => {
+const getContact =asyncHandler  (async (req, res) => {
     res.status(200).json({message: `Get contact for ${req.params.id}`})
-}
+})
 
 //@Description: Update Contact
 //@Route: PUT /api/contacts/:id
 //@Access: Public
-const updateContact = async (req, res) => {
+const updateContact = asyncHandler (async (req, res) => {
     res.status(200).json({message: `Update contact for ${req.params.id}`})
-}
+})
 
 //@Description: Delete Contact
 //@Route: DELETE /api/contacts/:id
 //@Access: Public
-const deleteContact = async (req, res) => {
+const deleteContact = asyncHandler (async (req, res) => {
     res.status(200).json({message: `Delete contact for ${req.params.id}`})
-}
+})
 
 //exprot the controller functions
 // module.exports = getContacts
