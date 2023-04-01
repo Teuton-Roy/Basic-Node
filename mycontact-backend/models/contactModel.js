906b1779d873c85283bf2e9b3474af488f1eb2cd
Teuton Roy <popeyehk404@gmail.com>
2023-04-01 13:26:57 +0530

    create contactModel Schema

---
+++ mycontact-backend/models/contactModel.js
@@ -0,0 +1,23 @@
+//import mongoose
+const mongoose = require('mongoose')
+
+//create schema
+const contactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide a name"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+    },
+    phone: {
+        type: String,
+        required: [true, "Please provide a phone number"],
+    },
+}, timeStamps = true);
+
+
+
+//exports model
+module.exports = mongoose.model('Contact', contactSchema)
