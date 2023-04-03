e507eb61ab2b362fcfb5c6b64687042e2309f7fb
Teuton Roy <popeyehk404@gmail.com>
2023-04-03 20:31:19 +0530

    ....

--- mycontact-backend/controllers/contactControllers.js
+++ mycontact-backend/controllers/contactControllers.js
@@ -29,6 +29,7 @@ const createContact = asyncHandler(async (req, res) => {
         phone,
         user_id: req.user.id
     })
+
     res.status(201).json(contact)
 })
 
--- mycontact-backend/models/contactModel.js
+++ mycontact-backend/models/contactModel.js
@@ -7,8 +7,8 @@ const contactSchema = new mongoose.Schema({
     //user id and this id will be for the user who created the contact
     user_id: {
         type: mongoose.Schema.Types.ObjectId,
+        required: true,
         ref: 'User',
-        required: true
     },
     name: {
         type: String,
