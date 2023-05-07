cefb2286284ec297334893e8cbdd7a923fa1b306
Teuton Roy <popeyehk404@gmail.com>
2023-05-07 19:39:24 +0530

    add new line in errorHandler

--- mycontact-backend/middleware/errorHandler.js
+++ mycontact-backend/middleware/errorHandler.js
@@ -4,6 +4,7 @@ const constants = require('../constants')
 //create constant error handler
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500
+    res.json({massage: err.message, stackTrace: err.stack})
 
     //set the status code using switch case
     switch (statusCode) {
