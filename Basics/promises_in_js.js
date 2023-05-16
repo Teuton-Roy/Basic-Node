48c27d7900107ac1157a9e3492f2cc0167bad416
Teuton Roy <popeyehk404@gmail.com>
2023-05-16 19:36:35 +0530

    About promise

---
+++ Basics/promises_in_js.js
@@ -0,0 +1,12 @@
+/* 
+    What is a promise?
+    
+    In simple example,
+    with our daily-life, that, we orderd food from Zomato and Swiggy, and they promised us to deliver the food within few minutes.
+    Then we do our task.
+
+    So, in JavaScript, 
+    Promise is a promise of code excution, that, it will be executed in future.
+    The code either executed successfully or failed. 
+    In both the cases, the user will be notified.
+*/
