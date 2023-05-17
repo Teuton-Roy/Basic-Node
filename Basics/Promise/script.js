431744a4bd723be238e462fa286aca40d37ba623
Teuton Roy <popeyehk404@gmail.com>
2023-05-17 19:37:47 +0530

    .catch()

--- Basics/Promise/script.js
+++ Basics/Promise/script.js
@@ -3,7 +3,7 @@ let promise = new Promise(function(resolve, reject){
     
     console.log("Promise is in pending state")
     setTimeout(()=>{
-        console.log('I am in Promise and I am resolve')
+        // console.log('I am in Promise and I am resolve')
         resolve(true)
     },5000)
 })
@@ -14,12 +14,18 @@ let promise1 = new Promise(function(resolve, reject){
     
     console.log("Promise is in pending state")
     setTimeout(()=>{
-        console.log('I am in Promise and I am rejected')
+        // console.log('I am in Promise and I am rejected')
         reject(new Error('I am rejected'))
   },5000)
 })
 // console.log(promise, promise1)
 
+//.then()
 promise.then((value) => {
     console.log(value)
+})
+
+//.catch()
+promise1.catch((error) => {
+    console.log('somthing went wrong in Promise1');
 })
