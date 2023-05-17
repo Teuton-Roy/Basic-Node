d9edca959b5298f2871ec7545d36a3780f135952
Teuton Roy <popeyehk404@gmail.com>
2023-05-17 19:28:31 +0530

    reject()

--- Basics/Promise/script.js
+++ Basics/Promise/script.js
@@ -1,9 +1,21 @@
+//resolve
+// let promise = new Promise(function(resolve, reject){
+    
+//     console.log("Promise is in pending state")
+//     setTimeout(()=>{
+//         console.log('I am in Promise and I am resolve')
+//         resolve(true)
+//     },5000)
+// })
+// console.log(promise)
+
+//Reject
 let promise = new Promise(function(resolve, reject){
     
     console.log("Promise is in pending state")
     setTimeout(()=>{
-        console.log('I am in Promise and I am resolve')
-        resolve(true)
-    },5000)
+        console.log('I am in Promise and I am rejected')
+        reject(new Error('I am rejected'))
+  },5000)
 })
 console.log(promise)
